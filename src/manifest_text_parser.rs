1573a46b15aba5aea2ea24dcb1c006c44caa87de
oScape <benoit.chassignol@gmail.com>
2020-01-23 14:34:33 +0100

    regex + lines to vec

--- src/manifest_text_parser.rs
+++ src/manifest_text_parser.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use std::cmp::PartialEq;
 
 pub struct ManifestTextParser {}
 
@@ -10,25 +11,61 @@ impl ManifestTextParser {
         Self {}
     }
 
-    pub fn parse_playlist(&self, data: String) {
+    pub fn parse_playlist(&self, data: String, absolutePlaylistUri: String) {
         // Normalize newlines to \n.
-        let lines = data.lines();
-        let reg_header = Regex::new("^#EXTM3U($|[ \t\n])").unwrap();
+        let data_normalized = data.replace("/\r\n|\r(?=[^\n]|$)/gm", "\n");
+        let data_splitted = data_normalized.trim().split("/\n+/m");
+        let lines: Vec<&str> = data_splitted.collect();
 
-        if !reg_header.is_match(lines.clone().nth(0).unwrap()) {
+        let reg_header = Regex::new("^#EXTM3U($|[ \t\n])").unwrap();
+        if !reg_header.is_match(lines.first().unwrap()) {
             println!("HLS header manifest is empty");
         }
 
-        let playlistType = PlaylistType::Master;
-        let mut skip = true;
+        let mut playlistType = PlaylistType::Master;
 
-        for line in lines {
+        // First, look for media playlist tags, so that we know what the playlist type really is before we start parsing.
+        // Whether to skip the next element; initialize to true to skip first elem.
+        let mut skip = true;
+        for line in lines.clone() {
+            println!("{:#}", line);
             // Ignore comments.
             if Self::is_comment(line) || skip {
                 skip = false;
                 continue;
             }
-            // let tag = Self::parse_tag(line);
+
+            let tag = Self::parse_tag(line);
+
+            if MEDIA_PLAYLIST_TAGS.iter().any(|t| t == &tag.name) {
+                playlistType = PlaylistType::Media;
+                break;
+            } else if tag.name == "EXT-X-STREAM-INF" {
+                skip = true;
+            }
+        }
+
+        let mut tags: Vec<Tag> = Vec::new();
+        skip = true;
+        for (i, line) in lines.iter().enumerate() {
+            // Ignore comments.
+            if Self::is_comment(line) || skip {
+                skip = false;
+                continue;
+            }
+
+            let tag = Self::parse_tag(line);
+            if MEDIA_PLAYLIST_TAGS.iter().any(|t| t == &tag.name) {
+                // match playlistType {
+                //     PlaylistType::Media => {
+                //         let segmentsData: Vec<&str> = lines[..i].into();
+                //         let segments =
+                //             Self::parse_segments(&absolutePlaylistUri, segmentsData, tags);
+                //         Playlist::new(absolutePlaylistUri.clone(), playlistType, tags, segments);
+                //     }
+                //     _ => println!("Only media playlists should contain segment tags"),
+                // }
+            }
         }
     }
 
@@ -36,13 +73,9 @@ impl ManifestTextParser {
      * Matches a string to an HLS comment format and returns the result.
      */
     fn is_comment(line: &str) -> bool {
-        if line.starts_with("#EXT") {
-            false
-        } else if line.starts_with("#") {
-            true
-        } else {
-            false
-        }
+        let reg_comment = Regex::new("^#").unwrap();
+        let reg_ext = Regex::new("^#EXT").unwrap();
+        reg_comment.is_match(line) && !reg_ext.is_match(line)
     }
 
     /**
@@ -63,18 +96,76 @@ impl ManifestTextParser {
         let data: &str = tag[tag.find(":").unwrap() + 1..].into();
 
         if data.len() > 0 {}
-        Tag::new()
+        Tag::new(name.to_owned())
+    }
+
+    /**
+     * Parses an array of strings into an array of HLS Segment objects.
+     */
+    fn parse_segments(
+        absoluteMediaPlaylistUri: &String,
+        lines: Vec<&str>,
+        playlistTags: Vec<Tag>,
+    ) -> Vec<Segment> {
+        let mut segments: Vec<Segment> = Vec::new();
+        segments.push(Segment::new());
+        segments
     }
 }
 
-struct Tag {}
+/**
+ * HLS tag struct.
+ */
+struct Tag {
+    name: String,
+}
 
 impl Tag {
+    pub fn new(name: String) -> Self {
+        Self { name: name }
+    }
+}
+
+/**
+ * HLS segment struct.
+ */
+struct Segment {}
+
+impl Segment {
     pub fn new() -> Self {
         Self {}
     }
 }
 
+/**
+ * HLS playlist struct.
+ */
+struct Playlist {
+    absoluteUri: String,
+    playlistType: PlaylistType,
+    tags: Vec<Tag>,
+    segments: Vec<Segment>,
+}
+
+impl Playlist {
+    pub fn new(
+        absoluteUri: String,
+        playlistType: PlaylistType,
+        tags: Vec<Tag>,
+        segments: Vec<Segment>,
+    ) -> Self {
+        Self {
+            absoluteUri: absoluteUri,
+            playlistType: playlistType,
+            tags: tags,
+            segments: segments,
+        }
+    }
+}
+
+/**
+ * Reads elements from strings.
+ */
 struct TextParser {}
 
 impl TextParser {
@@ -87,7 +178,7 @@ impl TextParser {
  * HLS tags that only appear on Media Playlists.
  * Used to determine a playlist type.
  */
-static MEDIA_PLAYLIST_TAGS: &[&str] = &[
+const MEDIA_PLAYLIST_TAGS: [&'static str; 7] = [
     "EXT-X-TARGETDURATION",
     "EXT-X-MEDIA-SEQUENCE",
     "EXT-X-DISCONTINUITY-SEQUENCE",
@@ -101,7 +192,7 @@ static MEDIA_PLAYLIST_TAGS: &[&str] = &[
  * HLS tags that only appear on Segments in a Media Playlists.
  * Used to determine the start of the segments info.
  */
-static SEGMENT_TAGS: &[&str] = &[
+const SEGMENT_TAGS: [&'static str; 6] = [
     "EXTINF",
     "EXT-X-BYTERANGE",
     "EXT-X-DISCONTINUITY",
